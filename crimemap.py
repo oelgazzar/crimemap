fb982766ab9a844b77f85cbf51533a005e4c286f
omar <omelgazzar@gmail.com>
2018-03-09 04:06:02 +0200

    Retrieve crimes from databases.

--- crimemap.py
+++ crimemap.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request, redirect, url_for
 import dbhelper
 
@@ -5,39 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	try:
-		data = dbhelper.get_all_inputs()
-	except Exception as e:
-		print(e)
-		data = None
-	return render_template("home.html", data=data)
+	crimes = dbhelper.get_all()
+	crimes = json.dumps(crimes)
+	return render_template("home.html", crimes=crimes)
 	
-@app.route("/add", methods=["POST"])
-def add():
+@app.route("/submitcrime", methods=["POST"])
+def submitcrime():
 	try:
 		data = request.form
-		# dbhelper.add_input(data)
-		print(data)
+		dbhelper.add_crime(data)
+		# print(data)
 	except Exception as e:
 		print(e)
 	return redirect(url_for("home"))
 
-@app.route("/clear")
-def clear_all():
-	try:
-		dbhelper.clear_all()
-	except Exception as e:
-		print(e)
-	return redirect(url_for("home"))
-
-@app.route("/clear/<id_>")
-def clear(id_):
-	try:
-		dbhelper.clear(id_)
-	except Exception as e:
-		print(e)
-	return redirect(url_for("home"))
-	
 if __name__ == "__main__":
 	app.run(debug=True)
 
--- dbhelper.py
+++ dbhelper.py
@@ -7,20 +7,29 @@ def _connect():
 						   passwd = dbconfig.db_password)
 	return conn
    
-def get_all_inputs():
+def get_all():
 	conn = _connect()
 	try:
 		with conn.cursor() as cursor:
-			cursor.execute("SELECT id, description FROM crimemap.crimes")
-			return cursor.fetchall()
+			cursor.execute("SELECT latitude, longitude, date, category, description FROM crimemap.crimes")
+			
+			keys = ("latitude", "longitude", "date", "category", "description")			
+			crimes = []
+			for crime in cursor.fetchall():
+				crime_dict = dict(zip(keys, crime))
+				crime_dict["date"] = crime_dict["date"].strftime("%F")
+				crimes.append(crime_dict)
+
+			return crimes
 	finally:
 		conn.close()
 		
-def add_input(data):
+def add_crime(data):
 	conn = _connect()
 	try:
 		with conn.cursor() as cursor:
-			cursor.execute("INSERT INTO crimemap.crimes (description) VALUES ('%s');" % data)
+			cursor.execute("""INSERT INTO crimemap.crimes (latitude, longitude, date, category, description)
+							  VALUES (%(latitude)s, %(longitude)s, %(date)s, %(category)s, %(description)s);""", data)
 			conn.commit()
 	finally:
 		conn.close()
@@ -38,7 +47,7 @@ def clear(id_):
 	conn = _connect()
 	try:
 		with conn.cursor() as cursor:
-			cursor.execute("DELETE FROM crimemap.crimes WHERE id = '%s'" % id_)
+			cursor.execute("DELETE FROM crimemap.crimes WHERE id = %s", id_)
 			conn.commit()
 	finally:
 		conn.close()
